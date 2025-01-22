10037cfc82f7a7a2e90471616c9ed09b708cda8b
Hanzala Sarguroh <hanzalasarguroh@gmail.com>
2025-01-22 19:11:27 +0530

    refactor: update static file serving and enhance socket event handling

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
@@ -25,12 +26,17 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
     io.emit("customEvent", "A user has connected");
+    socket.on("disconnect", () => {
+        console.log(`User disconnected: ${socket.id}`);
+        io.emit("customEvent", "A user has disconnected");
+    });
 });
 
-io.on("customEvent", (data) => {
-    console.log("Custom event received:", data);
-    io.emit("customEvent", "Hello from the server!");
+io.on("hello", (data) => {
+    console.log("Custom ", data);
+    // io.emit("customEvent", "Hello from the server!");
 });
+
 
 server.listen(3000, () => {
     console.log("Server is running on port 3000");
